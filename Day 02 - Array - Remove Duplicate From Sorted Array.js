9ce672525f0d4091cb9a29407e0c4fdf0388af16
Shivani <shivaniautar18@gmail.com>
2021-07-20 17:43:12 -0700

    Remove duplicate from sorted array

---
+++ Day 02 - Array - Remove Duplicate From Sorted Array.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var removeDuplicates = function(nums) {
+    if(nums.length == 0) return 0; //takes care of initial case if nothing in array
+    let i = 0; //2 runners i and j
+    for (let j=1; j< nums.length; j++){
+        if(nums[j] !== nums[i]){
+           i++;
+           nums[i] = nums[j];
+        }
+    }
+    return i + 1
+};
+
+
+// DIFFERENT APPROACH:
+
+ var removeDuplicates = function(nums) {
+    let runner = 1;
+    if(nums.length <= 1) return 0;
+    else {
+        for(let i=0; i<nums.length; i++){
+            if(nums[i]>nums[i-1]){
+                nums[runner]=nums[i]
+                runner++;
+            }
+    }
+    return runner;
+        
+    }
+};
