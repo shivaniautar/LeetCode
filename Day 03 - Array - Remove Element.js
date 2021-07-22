e2a701ba8e541e82c5ee0000ec4ad34ad4e7b0b5
Shivani <shivaniautar18@gmail.com>
2021-07-22 14:03:50 -0700

    remove element

---
+++ Day 03 - Array - Remove Element.js
@@ -0,0 +1,16 @@
+var removeElement = function(nums, val) {
+    for(let i=nums.length-1;i>=0;i--){
+        if(nums[i]===val) nums.splice(i,1)
+    }
+    return nums.length;
+};
+
+var removeElement = function(nums, val) {
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===val) {
+            nums.splice(i,1)
+            i--;
+        }
+    }
+    return nums.length;
+};
